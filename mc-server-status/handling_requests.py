b1d6e85b546dc195a071982b839f55a9406cdb46
Moritz344 <moritzmaier34@gmail.com>
2025-04-06 11:31:46 +0200

    feat: getting the data from the api

--- mc-server-status/handling_requests.py
+++ mc-server-status/handling_requests.py
@@ -10,25 +10,55 @@ def get_server_info(base,server) -> str:
         players_on = data["players"]["online"]
         players_max: max = data["players"]["max"]
         version = data["version"]["name_clean"]
-        player_list_on: list = []
-        status: bool = data["online"]
-        # maximale nummer die ich bekomme von der api
-        max_number: int = 12
 
+        player_list_on: list = []
+        mods_list: list = []
+        plugins_list: list = []
+
+        status: bool = data["online"]
+        motd = data["motd"]["clean"]
+
+        mods = data["mods"]
+        plugins = data["plugins"]
+
+        port: int = data["port"]
+        eula_blocked = data["eula_blocked"]
+
+        max_number: int = 12
+        
+        # O.O
         if players_on < 100:
-            for index in range(0,players_on ):
+            for index in range(0,players_on):
                 if index < max_number:
-                    name = data["players"]["list"][index]["name_clean"]
-                    player_list_on.append(name)
+                    try:
+                        name = data["players"]["list"][index]["name_clean"]
+                        player_list_on.append(name)
+                    except Exception:
+                        player_list_on = []
                 else:
                     continue
 
+        for index in range(0,len(mods )):
+            try:
+                mod_name = data["mods"][index]["name"]
+                mods_list.append(mod_name)
+            except Exception as e:
+                mods_list = []
+                print("There was an error while loading the mods.",e)
 
-        return players_on,players_max,player_list_on,status,version
+        for index in range(0,len(plugins)):
+            try:
+                plugin_name = data["plugins"][index]["name"]
+                plugins_list.append(plugin_name)
+            except Exception as e:
+                plugins_list = []
+                print("There was an error while loading the plugins.",e)
+
+        return players_on,players_max,player_list_on,status,version,motd,mods_list,port,eula_blocked,host,plugins_list
 
         
 
     else:
-        print(response.status_code)
+        print("Status Code:",response.status_code)
 
     
