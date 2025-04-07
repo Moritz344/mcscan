8ce8b3efc8ea76533cbf02a5f3ef0cc60c301586
Moritz344 <moritzmaier34@gmail.com>
2025-04-07 19:57:03 +0200

    feat: new display choices

--- mc_server_status/format_data.py
+++ mc_server_status/format_data.py
@@ -3,8 +3,13 @@ from rich.panel import Panel
 from rich.console import Console
 from termcolor import cprint,colored
 from rich.box import *
-import rich.layout
+from rich.markdown import Markdown
+from logging_handler import *
+from rich.prompt import Prompt
+import questionary
 
+# TODO: ascii art, keine einzelnen panels benutzen
+# TODO: Logging Handler
 
 
 def get_data(players_on,players_max,players_list_on,status,version,motd,mods_list,port,eula_blocked,host,plugins_list):
@@ -12,13 +17,33 @@ def get_data(players_on,players_max,players_list_on,status,version,motd,mods_lis
 
     players_list_on = ",".join(players_list_on)
     
-    status_color = "[green]"
+    status_color = "[bold green]"
+    mods_color = "[white]"
+    plugins_color = "[white]"
+    eula_color = "[white]"
+    
+
     if status:
         status = "Online"
-        status_color = "[green]"
+        status_color = "[bold green]"
     else:
         status = "Offline"
-        status_color = "[red]"
+        status_color = "[bold red]"
+
+    if mods_list :
+        mods_color = "[green]"
+    else:
+        mods_color = "[#3c3836]"
+
+    if plugins_list:
+        plugins_color = "[green]"
+    else:
+        plugins_color = "[#3c3836]"
+
+    if eula_blocked:
+        eula_color = "[green]"
+    else:
+        eula_color = "[red]"
 
     motd = motd.split()
     motd = "  ".join(motd)
@@ -38,19 +63,61 @@ def get_data(players_on,players_max,players_list_on,status,version,motd,mods_lis
     if plugins_list == "":
         plugins_list = "N/A"
 
-    p0 = rich.panel.Panel(f"[magenta]Status:      [/magenta]    {status_color}{status}",box=rich.box.ROUNDED,highlight=True,)
-    p1 = rich.panel.Panel(f"[magenta]Host:        [/magenta]    {host}",box=rich.box.ROUNDED,highlight=True,)
-    p2 = rich.panel.Panel(f"[magenta]Port:        [/magenta]    {status_color}{port}",box=rich.box.ROUNDED,highlight=True,)
-    p3 = rich.panel.Panel(f"[magenta]MOTD:        [/magenta]    [blue]{motd}",box=rich.box.ROUNDED,highlight=True,)
-    p4 = rich.panel.Panel(f"[magenta]Version:     [/magenta]    [blue]{version}",box=rich.box.ROUNDED,highlight=True,)
-    p5 = rich.panel.Panel(f"[magenta]Players:     [/magenta]    {players_on}/{players_max}", box=rich.box.ROUNDED,highlight=True)
+    answers = questionary.form(
+            select = questionary.select("How would you like to display the data?",choices=["Markdown 🧙‍♂️","Panel 📦"],)
 
-    p6 = rich.panel.Panel(f"[magenta]Mods:        [/magenta]    {mods_list}", box=rich.box.ROUNDED,highlight=True)
-    p7 = rich.panel.Panel(f"[magenta]Plugins:     [/magenta]    {plugins_list}", box=rich.box.ROUNDED,highlight=True)
-    p8 = rich.panel.Panel(f"[magenta]EULA Blocked:[/magenta]    {eula_blocked}/{eula_blocked}", box=rich.box.ROUNDED,highlight=True)
+    ).ask()
 
-    p9 = rich.panel.Panel(f"[magenta]Player list: [/magenta]    {players_list_on}", box=rich.box.ROUNDED,highlight=True)
-    console.clear()
-    console.print(p0,p1,p2,p3,p4,p5,p6,p7,p8)
+    selected_item = answers["select"]
+    if selected_item == "Markdown 🧙‍♂️":
+        MARKDOWN = f"""
+        # MARKDOWN FORMAT
 
+        Status:        {status}
+        Host:          {host}
+        Port:          {port}
+        MOTD:          {motd}
+        Version:       {version}
+        Players:       {players_on}/{players_max}
+
+        Mods:          {mods_list}
+        Plugins:       {plugins_list}
+        EULA Blcocked: {eula_blocked}
+        Player List:   {players_list_on}
+
+        
+        """
+        console.clear()
+        md = Markdown(MARKDOWN)
+        console.print(md)
+    elif selected_item == "Panel 📦":
+        panel = Panel(
+                f"[bold yellow]Status:           [/]{status_color}{status}[/]"
+                "\n"
+                f"[bold yellow]Host:             [/][#458588]{host}[/]"
+                "\n"
+                f"[bold yellow]Port:             [/][#d65d0e]{port}[/]"
+                "\n"
+                f"[bold yellow]MOTD:             [/][#ebdbb2]{motd}[/]"
+                "\n"
+                f"[bold yellow]Version:          [/][#689d6a]{version}[/]"
+                "\n"
+                f"[bold yellow]Players:          [/][#a89984]{players_on}/{players_max}[/]"
+                "\n"
+                f"[bold yellow]Mods:             [/]{mods_color}{mods_list}[/]"
+                "\n"
+                f"[bold yellow]Plugins:          [/]{plugins_color}{plugins_list}[/]"
+                "\n"
+                f"[bold yellow]EULA Blocked:     [/]{eula_color}{eula_blocked}[/]"
+                "\n"
+                f"[bold yellow]Player list:      [/]{players_list_on}",
+
+                title="Panel",
+                title_align="right",
+                border_style="bold blue",
+        )
+
+
+        console.clear()
+        console.print(panel)
 
